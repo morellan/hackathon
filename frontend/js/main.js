c908dbd8f3687736423088759ce15fb8277bb698
Andrés Franzoy <andres@franzoy.cl>
2012-10-11 20:57:12 -0300

    Backbone 	new file:   frontend/css/bootstrap.css 	new file:   frontend/css/bootstrap.min.css 	new file:   frontend/css/main.css 	new file:   frontend/img/glyphicons-halflings-white.png 	new file:   frontend/img/glyphicons-halflings.png 	new file:   frontend/index.html 	new file:   frontend/js/backbone.js 	new file:   frontend/js/bootstrap.js 	new file:   frontend/js/bootstrap.min.js 	new file:   frontend/js/jquery-1.8.1.min.js 	new file:   frontend/js/main.js 	new file:   frontend/js/underscore.js

---
+++ frontend/js/main.js
@@ -0,0 +1,77 @@
+// Setup
+
+var Hackathon = {
+	Models: {},
+	Collections: {},
+	Views: {},
+	Instances: {},
+	Variables: {},
+}
+
+// Models
+
+Hackathon.Models.User = Backbone.Model.extend({	
+});
+
+// Collections
+
+Hackathon.Collections.Users = Backbone.Collection.extend({
+	model: Hackathon.Models.User,
+	url: 'http://192.168.1.111:4567'
+});
+
+// Views
+
+Hackathon.Views.UserList = Backbone.View.extend({
+	el: '#lista',
+
+	initialize: function() {
+		this.collection = new Hackathon.Collections.Users();
+		this.collection.bind('reset',this.render,this);
+		this.collection.bind('add',this.render,this);
+		this.collection.fetch();
+	},
+
+	render: function() {
+		$(this.el).html("");
+		var self = this;
+		self.collection.each(function(model){
+			self.addOne(model);
+		});
+	},
+
+	addOne: function(model) {
+		var view = new Hackathon.Views.UserListItem({model: model});
+		$(this.el).append(view.render().el);
+	},
+});
+
+Hackathon.Views.UserListItem = Backbone.View.extend({
+	template: _.template($('#tpl-tr').html()),
+
+	initialize: function(){
+		this.model.bind("change",this.render,this);
+	},
+
+	render: function(){
+		$(this.el).html(this.template(this.model.toJSON()));
+		return this;
+	}
+})
+
+// Router
+Hackathon.Router = Backbone.Router.extend({
+
+    routes: {
+        "": "initialize"
+    },
+
+    initialize: function () {
+    	var view = new Hackathon.Views.UserList();
+    },
+});
+
+$(function(){
+	var router = new Hackathon.Router();
+	Backbone.history.start();
+})
